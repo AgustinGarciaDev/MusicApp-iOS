500072e34e7fb6f40596a88fbd2d12df354712f6
Agustin garcia <agustinchinchilla16@gmail.com>
2021-10-23 10:29:14 -0300

    Initial Commit

---
+++ App reproductor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App reproductor
+//
+//  Created by Agustin Chinchilla on 23/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
